49278578500b5b86f79738a8ca696194b742a076
Doreen Brinkman <dbrinkman@esri.com>
2024-05-24 14:22:42 -0700

    split button

--- src/main.jsx
+++ src/main.jsx
@@ -2,44 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { setAssetPath } from '@esri/calcite-components/dist/components';
 import "@esri/calcite-components/dist/calcite/calcite.css";
-import "@esri/calcite-components/dist/components/calcite-tabs";
-import "@esri/calcite-components/dist/components/calcite-tab-nav";
-import "@esri/calcite-components/dist/components/calcite-tab-title";
-import "@esri/calcite-components/dist/components/calcite-tab";
-import "@esri/calcite-components/dist/components/calcite-notice";
+import "@esri/calcite-components/dist/components/calcite-modal";
+import "@esri/calcite-components/dist/components/calcite-split-button";
+import "@esri/calcite-components/dist/components/calcite-dropdown-item";
+import "@esri/calcite-components/dist/components/calcite-dropdown-group";
 import {
-  CalciteTabs,
-  CalciteTabNav,
-  CalciteTabTitle,
-  CalciteTab,
-  CalciteNotice,
+  CalciteModal,
+  CalciteSplitButton,
+  CalciteDropdownItem,
+  CalciteDropdownGroup,
 } from "@esri/calcite-components-react";
 
 setAssetPath(location.href);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CalciteTabs>
-      <CalciteTabNav slot="title-group">
-        <CalciteTabTitle selected>Watercraft</CalciteTabTitle>
-        <CalciteTabTitle>Automobiles</CalciteTabTitle>
-        <CalciteTabTitle>Aircrafts</CalciteTabTitle>
-      </CalciteTabNav>
-      <CalciteTab selected>
-        <CalciteNotice icon="embark" open>
-          <div slot="message">Recommended for coastal use</div>
-        </CalciteNotice>
-      </CalciteTab>
-      <CalciteTab>
-        <CalciteNotice icon="car" open>
-          <div slot="message">A good choice for inland adventure</div>
-        </CalciteNotice>
-      </CalciteTab>
-      <CalciteTab>
-        <calcite-notice icon="plane" open>
-          <div slot="message">Cross continents quickly</div>
-        </calcite-notice>
-      </CalciteTab>
-    </CalciteTabs>
+    <CalciteModal
+      width="s"
+      scale="m"
+      widthScale="s"
+      open
+      outsideCloseDisabled
+      dismissible>
+        <div slot="header">Safari split button</div>
+        <div slot="content">Add some content here</div>
+        <CalciteSplitButton
+          slot="secondary"
+          primaryText="Save and leave"
+          scale="m">
+          <CalciteDropdownGroup selectionMode="none">
+            <CalciteDropdownItem>Save and leave</CalciteDropdownItem>
+            <CalciteDropdownItem>Save as</CalciteDropdownItem>
+            <CalciteDropdownItem>Save option</CalciteDropdownItem>
+          </CalciteDropdownGroup>
+        </CalciteSplitButton>
+      </CalciteModal>
   </React.StrictMode>
 );
